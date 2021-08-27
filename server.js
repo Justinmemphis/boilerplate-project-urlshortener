88540b156a63e5503f286aee25268192765dd3a5
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-27 10:06:34 -0500

    Update server.js
    initial project setup

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+/*
+TO DO:
+  set up variables
+  test if URL is a valid url
+    1. if not a valid URL - give error message
+        otherwise move to next step
+        (in here save to MongoDB - and retrieve number saved)
+    2. generate JSON response - original URL and short url
+    3. when you visit the short URL you get redirected to the original URL
+
+*/
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+var bodyParser = require('body-parser');
+
+// enable bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,9 +31,15 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+/*
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+*/
+
+app.get("/api", function (req, res, next) {
+
 });
 
 app.listen(port, function() {
