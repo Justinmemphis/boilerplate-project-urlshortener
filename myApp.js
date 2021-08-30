69201416ce01b4f809d9de35e584290ca3d37485
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-30 14:47:26 -0500

    Update myApp.js
    move comments from server.js

--- myApp.js
+++ myApp.js
@@ -1,3 +1,38 @@
+/*
+TO DO:
+
+08/30:
+    1. Get new record to post to MongoDB
+        A. Why is MongDB creating a sub-database vs. collection database?
+
+
+  set up variables
+  test if URL is a valid url
+    1. if not a valid URL - give error message
+        otherwise move to next step
+        (in here save to MongoDB - and retrieve number saved)
+    2. generate JSON response - original URL and short url
+    3. when you visit the short URL you get redirected to the original URL
+
+08/27 Update - got mongoose initially installed (maybe) - need to get it to
+correctly post and save new URL record below - that will mean it is working
+
+Also - need to test URL when submitted to make sure it is a valid Url
+Additionally, once that is done, need to test URL to see if it is already
+in the database or not.  If it's in the database go ahead and pull the shortUrl
+already generated.  If it's not in the database create a new record
+
+Finally, need to have the /api/shorturl redirect browser to the given location
+(assuming we also create a new short url for this as well)
+
+Sources:
+1. This is where it talks about catching route problems:
+https://stackoverflow.com/questions/44539210/express-js-handle-unmached-routes
+
+
+
+*/
+
 require('dotenv').config();
 
 const mongoose = require('mongoose');
