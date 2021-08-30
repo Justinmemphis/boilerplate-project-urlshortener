6223d4610b49216bc10148a5e282baa936da1ad4
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-30 14:03:58 -0500

    Create myApp.js
    created new myApp.js

---
+++ myApp.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+  url: { type: String, required: true},
+  shortUrl: Number
+});
+
+const Url = mongoose.model("Url", urlSchema);
+
+const createAndSaveUrl = (done) => {
+  const amazon = new Url({url: "https://www.amazon.com", shortUrl: 1});
+
+  amazon.save( (err, data) => {
+    if (err) return console.error(err);
+    done(null, data)
+  });
+};
+
+
+
+// exports at the bottom
+exports.UrlModel = Url;
+exports.createAndSaveUrl = createAndSaveUrl;
